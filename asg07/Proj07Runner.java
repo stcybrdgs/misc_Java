30a89f72e9f6d1691993c485f264b0d4caafcde0
Stacy Bridges <stcybrdgs@users.noreply.github.com>
2015-08-31 22:44:29 -0500

    Update Proj07Runner.java

--- asg07/Proj07Runner.java
+++ asg07/Proj07Runner.java
@@ -1,8 +1,7 @@
 /******************************************
 * Student:  Stacy Bridges
-* Course:   ITSE2317 - Java Programming (Intermediate)
+* Course:   Java Programming (Intermediate)
 * Assg:     Asg07
-* Date:     July 08, 2015
 *******************************************/
 import java.util.*;
 import javax.swing.*;
@@ -10,40 +9,41 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Proj07Runner extends JFrame implements ActionListener{
-	JFrame frame;
-	JButton leftButton;
-	JButton rightButton;
-	JLabel label;
+    JFrame frame;
+    JButton leftButton;
+    JButton rightButton;
+    JLabel label;
 	
-	public Proj07Runner(){
-		this.setTitle("Stacy Bridges");
-		this.setSize(250, 100);		// (width, height);
-		this.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    public Proj07Runner(){
+        this.setTitle("Stacy Bridges");
+	this.setSize(250, 100);		// (width, height);
+	this.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		
-		FlowLayout myLayout = new FlowLayout(FlowLayout.TRAILING);
-		this.setLayout(myLayout);
+	FlowLayout myLayout = new FlowLayout(FlowLayout.TRAILING);
+	this.setLayout(myLayout);
 		
-		leftButton = new JButton("Left");
-		label = new JLabel("Left", JLabel.CENTER);		
-		label.setForeground(Color.red);
-		label.setBackground(Color.yellow);
-		// rem: JLabel is transparent by default, so set it opaque
-		//		to show background color
-		label.setOpaque(true);				
-		rightButton = new JButton("Right");		
+	leftButton = new JButton("Left");
+	label = new JLabel("Left", JLabel.CENTER);		
+	label.setForeground(Color.red);
+	label.setBackground(Color.yellow);
+	// rem: JLabel is transparent by default, so set it opaque
+	// to show background color
+	label.setOpaque(true);				
+	rightButton = new JButton("Right");		
 		
-		this.add(leftButton);	
-		this.add(label);			
-		this.add(rightButton);
+	this.add(leftButton);	
+	this.add(label);			
+	this.add(rightButton);
 		
-		leftButton.addActionListener(this);
-		rightButton.addActionListener(this);
+        leftButton.addActionListener(this);
+	rightButton.addActionListener(this);
 		
-		setVisible(true);
-	}// end constructor
+	setVisible(true);
+    }// end constructor
 	
-	public void actionPerformed(ActionEvent event){
-		String buttonVal = event.getActionCommand();
-		label.setText(buttonVal);
-	}
+    public void actionPerformed(ActionEvent event){
+        String buttonVal = event.getActionCommand();
+	label.setText(buttonVal);
+    }
+    
 } // end class Proj07Runner
