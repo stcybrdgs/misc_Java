c51207be41f93ed7815b7303bc27da3806450428
Stacy Bridges <stcybrdgs@users.noreply.github.com>
2015-08-31 22:51:30 -0500

    Update Proj08Runner.java

--- asg08/Proj08Runner.java
+++ asg08/Proj08Runner.java
@@ -10,54 +10,53 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Proj08Runner extends JFrame{
-	JFrame frame;
-	JPanel panel;
-	JLabel label;
-	JSlider slider;
+    JFrame frame;
+    JPanel panel;
+    JLabel label;
+    JSlider slider;
 	
-	public Proj08Runner(){
-		this.setTitle("Stacy Bridges");
-		this.setSize(300, 100);		// (width, height);
-		this.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    public Proj08Runner(){
+        this.setTitle("Stacy Bridges");
+	this.setSize(300, 100);		// (width, height);
+	this.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		
-		//FlowLayout myLayout = new FlowLayout(FlowLayout.CENTER);
-		BorderLayout myLayout = new BorderLayout();
-		this.setLayout(myLayout);
+	// FlowLayout myLayout = new FlowLayout(FlowLayout.CENTER);
+	BorderLayout myLayout = new BorderLayout();
+	this.setLayout(myLayout);
 
-		// the sample program seems to place the counter label
-		//		such that it overlaps the slider a bit, so I'm
-		// 		adding a panel to the frame and then placing
-		//		my counter label in the panel. . . this 
-		//		mechanism provides the overlap necessary to mimic
-		//		the sample program. . . 
-		panel = new JPanel();					//	make the panel
-		this.add(panel, BorderLayout.NORTH);	//	add it to the frame
-		label = new JLabel("50", JLabel.CENTER);// 	make a counter label
-		panel.add(label);						//  add it to the panel
+	// the sample program seems to place the counter label
+	// such that it overlaps the slider a bit, so I'm
+	// adding a panel to the frame and then placing
+	// my counter label in the panel. . . this 
+	// mechanism provides the overlap necessary to mimic
+	// he sample program. . . 
+	panel = new JPanel();                       // make the panel
+	this.add(panel, BorderLayout.NORTH);        // add it to the frame
+	label = new JLabel("50", JLabel.CENTER);    // make a counter label
+	panel.add(label);                           // add it to the panel
 		
-		// add a slider to the frame
-		slider = new JSlider();
-		slider = new JSlider();
+	// add a slider to the frame
+	slider = new JSlider();
+	slider = new JSlider();
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
         slider.setMajorTickSpacing(10);
         slider.setMinorTickSpacing(5);
-		this.add(slider, BorderLayout.CENTER);	
+	this.add(slider, BorderLayout.CENTER);	
 		
-		// add a listener to the slider
-		slider.addChangeListener(new MyListener());
+        // add a listener to the slider
+	slider.addChangeListener(new MyListener());
 		
-		setVisible(true);
-	}// end constructor
+	setVisible(true);
+    } // end constructor
 	
-class MyListener implements ChangeListener{
-	public void stateChanged(ChangeEvent event){
-		
-		JSlider obj = (JSlider)event.getSource();	// make an obj handler
-		int x = obj.getValue();						// get value of  slider
-		label.setText(Integer.toString(x));			// print value to label
-	}// end stateChanged()	
-}// end class MyListener
+    class MyListener implements ChangeListener{
+        public void stateChanged(ChangeEvent event){
+            JSlider obj = (JSlider)event.getSource();    // make an obj handler
+	    int x = obj.getValue();                      // get value of slider
+	    label.setText(Integer.toString(x));		 // print value to label
+        } // end stateChanged()	
+    } // end class MyListener
 	
 } // end class Proj08Runner
 
